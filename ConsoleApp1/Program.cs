0a99393bffaa6fd838018fd0301fec797d076e09
Lexicon <Lexicon@LAPTOP-SQ8C44R4>
2021-12-27 18:46:51 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( ; ; )
+            {
+                string equation = Console.ReadLine();
+                if (equation.Contains("x"))
+                {
+                    Environment.Exit(0); 
+                }
+               else if (equation.Contains("+"))
+                {
+
+                    string[] values = equation.Split('+');
+                    int result = int.Parse(values[0]) + int.Parse(values[1]);
+                    Console.WriteLine(result);
+                    Console.WriteLine(".............");
+                }
+                else if (equation.Contains("-"))
+                {
+
+                    string[] values = equation.Split('-');
+                    int result = int.Parse(values[0]) - int.Parse(values[1]);
+                    Console.WriteLine(result);
+                    Console.WriteLine(".............");
+                }
+                else if (equation.Contains("*"))
+                {
+
+                    string[] values = equation.Split('*');
+                    int result = int.Parse(values[0]) * int.Parse(values[1]);
+                    Console.WriteLine(result);
+                    Console.WriteLine(".............");
+                }
+                else if (equation.Contains("/"))
+                {
+                    string[] values = equation.Split('/');
+                    if ((values[1]) == null)
+                    {
+                        throw new DivideByZeroException("Can Not Divide By Zero");
+                    }
+                    {
+                    
+                      int result = int.Parse(values[0]) / int.Parse(values[1]);
+                      Console.WriteLine(result);
+                      Console.WriteLine(".............");
+                    }
+                }
+            }
+
+        }
+    }
+}
